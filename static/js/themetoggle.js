a113515bbaace0f5d15f920a99aff76f56bd8639
Restyled.io <commits@restyled.io>
2022-06-25 17:19:11 +0000

    Restyled by prettier

--- static/js/themetoggle.js
+++ static/js/themetoggle.js
@@ -3,13 +3,13 @@ function setTheme(mode) {
   if (mode === "dark") {
     document.getElementById("darkModeStyle").disabled = false;
     document.getElementById("dark-mode-toggle").innerHTML =
-        "<i data-feather=\"sun\"></i>";
-    feather.replace()
+      '<i data-feather="sun"></i>';
+    feather.replace();
   } else if (mode === "light") {
     document.getElementById("darkModeStyle").disabled = true;
     document.getElementById("dark-mode-toggle").innerHTML =
-        "<i data-feather=\"moon\"></i>";
-    feather.replace()
+      '<i data-feather="moon"></i>';
+    feather.replace();
   }
 }
 
